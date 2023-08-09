1a23dd09a1ecbbdc34039653ca9e3268169c3a68
wilber4c <nsengiyumvawilberforce@gmail.com>
2023-08-09 15:54:31 +0300

    guest role created

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -627,7 +627,7 @@ $result = json_decode($this->custom->run_curl_get($url));
 			array_push($followup_family_savings, $value->count);
 		}
 
-		$region_and_district_url = API_BASE_URL . 'entries/group-by-region-and-districts?data_type=followup&form_id=11';
+		$region_and_district_url = API_BASE_URL . 'entries/group-by-region-and-districts?data_type=followup&form_id=11&startdate='.$startdate.'&enddate='.$enddate;
 		$region_and_district_result = json_decode($this->custom->run_curl_get($region_and_district_url))->data;
 		
 		$data['baseline_region'] = json_encode($baseline_region);
