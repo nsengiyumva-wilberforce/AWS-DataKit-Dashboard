6e2937aedb1dd43f386c1448349250b0da43169d
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-12-09 18:44:42 +0300

    new update

--- aws.api/app/Controllers/Entry.php
+++ aws.api/app/Controllers/Entry.php
@@ -512,17 +512,29 @@ class Entry extends BaseController
 			$end_date_received = explode('-', $params['dates'])[1];
 
 			$date1 = explode('/', $start_date_received);
-			$start_date = trim($date1[2]).'-'.trim($date1[0]).'-'.trim($date1[1]);
+			$start_date = trim($date1[2]) . '-' . trim($date1[0]) . '-' . trim($date1[1]);
 
 			$date2 = explode('/', $end_date_received);
-			$end_date = trim($date2[2]).'-'.trim($date2[0]).'-'.trim($date2[1]);
+			$end_date = trim($date2[2]) . '-' . trim($date2[0]) . '-' . trim($date2[1]);
 
-		
+
 			$query['responses.created_at'] = [
 				'$gte' => $start_date,
 				'$lte' => $end_date
 			];
-		} 
+		}
+
+		if (!empty($params['search'])) {
+
+			$searchTerm = $params['search'];
+
+			$query['$or'] = [
+				['responses.qn4' => ['$regex' => $searchTerm, '$options' => 'i']], // Another field
+				['responses.qn7' => ['$regex' => $searchTerm, '$options' => 'i']], // Another field
+				['responses.qn8' => ['$regex' => $searchTerm, '$options' => 'i']], // Another field
+				['responses.qn9' => ['$regex' => $searchTerm, '$options' => 'i']], // Another field
+			];
+		}
 		// Pagination parameters
 		$start = isset($params['start']) ? (int) $params['start'] : 0; // Using start instead of page
 		$perPage = isset($params['length']) ? (int) $params['length'] : 10; // Using length for perPage
@@ -567,6 +579,7 @@ class Entry extends BaseController
 
 		// Process data
 		$form_titles = $utility->form_titles($params['form_id']);
+
 		$dataGenetration = [];  // Initialize the array
 
 		$new_data = array_map(function ($entry) use ($form_titles, $user_map, &$dataGenetration) {
@@ -602,7 +615,7 @@ class Entry extends BaseController
 				'sub_title' => $entry['sub_title'],
 				'creator_id' => $entry['creator_id'],
 				'location' => $entry['district'] . ', ' . $entry['sub_county'] . ', ' . $entry['parish'] . ', ' . $entry['village'],
-				'last_follower' => $entry['last_follower']??'',
+				'last_follower' => $entry['last_follower'] ?? '',
 				'updated_at' => date('M j, Y', strtotime($entry['updated_at'] ?? $entry['created_at'])),
 				'response_id' => $entry['response_id'],
 			];
